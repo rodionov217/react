d6c8110a229d1802412354fd537c0a5ebf54c258
rodionov217 <rodionova217@gmail.com>
2018-09-20 14:44:36 +0300

    first

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,55 @@
 'use strict';
+const AuthForm = ({onAuth}) => {
+  let name, email, password;
+
+  const passwordRegEx =  /\w/gi;
+  const emailRegEx = /.|-|@|\w/gi;
+  const onChange = event => {
+    const input = event.nativeEvent.target;
+    if (input.type === 'email') {
+      if (!event.nativeEvent.data.match(emailRegEx)) {
+        input.value = input.value.slice(0, -1);
+      }
+      email = input.value;
+    } else if (input.type === 'password') {
+      if (!event.nativeEvent.data.match(passwordRegEx)) {
+        input.value = input.value.slice(0, -1);
+      } 
+      password = input.value;
+    } else {
+      name = input.value;
+    }
+  }
+  const onSubmit = event => {
+    event.preventDefault();
+    const user = {
+      name: name, 
+      email: email, 
+      password: password
+    }
+    if (onAuth && typeof onAuth === 'function') {
+      onAuth(user);
+    }
+  }
+  
+  return (
+    <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={onSubmit}>
+      <div className="Input">
+        <input required type="text" placeholder="Имя" onChange={onChange}/>
+        <label></label>
+      </div>
+      <div className="Input">
+        <input type="email" placeholder="Электронная почта" onChange={onChange}/>
+        <label></label>
+      </div>
+      <div className="Input">
+        <input required type="password" placeholder="Пароль" onChange={onChange}/>
+        <label></label>
+      </div>
+      <button type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right"></i>
+      </button>
+    </form>
+  )
+}
--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const FeedbackForm = ({data, onSubmit}) => {
+  let form;
+  const submit = event => {
+    event.preventDefault();
+    const feedback = {
+      salutation: form.querySelector('input[type=radio]:checked').value,
+      name: form.querySelector('input[type=text]').value,
+      subject: form.querySelector('select').value,
+      message: form.querySelector('textarea').value,
+      email: form.querySelector('input[type=email]').value,
+      snacks: form.querySelector('input[type=checkbox]:checked').value
+    }
+    onSubmit(JSON.stringify(feedback));
+  }
+
+  return (
+    <form ref={el => form = el} className="content__form contact-form" onSubmit={submit} >
+  <div className="testing">
+    <p>Чем мы можем помочь?</p>
+  </div>
+  <div  className="contact-form__input-group">
+    <input defaultChecked={data.salutation === 'Мистер'} className="contact-form__input contact-form__input--radio" id="salutation-mr" name="salutation" type="radio" value="Мистер"/>
+    <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
+    <input defaultChecked={data.salutation === 'Мисис'} className="contact-form__input contact-form__input--radio" id="salutation-mrs" name="salutation" type="radio" value="Мисис" />
+    <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
+    <input defaultChecked={data.salutation === 'Мис'} className="contact-form__input contact-form__input--radio" id="salutation-ms" name="salutation" type="radio" value="Мис" />
+    <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
+  </div>
+  <div className="contact-form__input-group">
+    <label className="contact-form__label"  htmlFor="name">Имя</label>
+    <input className="contact-form__input contact-form__input--text" id="name" name="name" type="text" placeholder={data.name || ''}/>
+  </div>
+  <div className="contact-form__input-group">
+    <label className="contact-form__label"  htmlFor="email">Адрес электронной почты</label>
+    <input className="contact-form__input contact-form__input--email" id="email" name="email" type="email" placeholder={data.email || ''}/>
+  </div>
+  <div className="contact-form__input-group">
+    <label className="contact-form__label"  htmlFor="subject">Чем мы можем помочь?</label>
+    <select className="contact-form__input contact-form__input--select" id="subject" name="subject" defaultValue={data.subject}>
+      <option >У меня проблема</option>
+      <option >У меня важный вопрос</option>
+    </select>
+  </div>
+  <div className="contact-form__input-group">
+    <label className="contact-form__label"  htmlFor="message">Ваше сообщение</label>
+    <textarea className="contact-form__input contact-form__input--textarea" id="message" name="message" rows="6" cols="65" placeholder={data.message || ''}></textarea>
+  </div>
+  <div className="contact-form__input-group">
+    <p className="contact-form__label--checkbox-group">Хочу получить:</p>
+    <input defaultChecked={data.snacks[0] === 'пицца'} className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца"/>
+    <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
+    <input defaultChecked={data.snacks[0] === 'пирог'} className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог"/>
+    <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
+  </div>
+  <button className="contact-form__button" type="submit">Отправить сообщение!</button>
+  <output id="result" />
+</form>
+  )
+}
