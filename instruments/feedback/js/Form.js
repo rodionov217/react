b5b48d10d42f15d1a7e8168b1c7110b6aee30d9a
rodionov217 <rodionova217@gmail.com>
2019-04-15 01:29:22 +0300

    hw

---
+++ instruments/feedback/js/Form.js
@@ -0,0 +1,54 @@
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: '',
+      text: ''
+    }
+
+    this.send = this.send.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  send() {
+    console.log("Name:", this.state.name, "Text:", this.state.text);
+    this.setState({name: '', text: ''})
+  }
+
+  handleChange(e) {
+    this.setState({ 
+      [e.target.name]: e.target.value
+     });
+  }
+
+  render() {
+    const {name, text} = this.state;
+    const done = name.length > 0 && text.length > 0;
+
+    return (
+      <main className="container">
+        <div className="row">
+          <section className="col-ls-6 offset-lg-3 col-sm-12">
+            <div className="form">
+              <h2>Обратная связь</h2>
+              <p>Нам важно ваше мнение</p>
+
+              <label>
+                Имя
+                <input className="input" name="name" value={this.state.name} onChange={this.handleChange} />
+              </label>
+
+              <label>
+                Сообщение
+                <textarea className="input" name="text" value={this.state.text} onChange={this.handleChange}></textarea>
+              </label>
+
+              <button className={`form__send ${done ? '' : 'form__send-disabled'}`} onClick={done ? this.send : null}>Отправить</button>
+            </div>
+          </section>
+        </div>
+      </main>
+    );
+  }
+}
---
+++ instruments/mortgage/js/Select.js
@@ -0,0 +1,11 @@
+const {Select} = Antd;
+const Autocomplete = () => (
+  <div>
+    Тип квартиры:<br/>
+    <select>
+      <option>Квартира в новостройке</option>
+      <option>Готовая квартира</option>
+      <option>Загородный дом</option>
+    </select>
+   </div>
+);
